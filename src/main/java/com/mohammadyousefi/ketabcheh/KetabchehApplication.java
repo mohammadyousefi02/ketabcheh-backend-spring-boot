fc290481c071d6874b4185547900365b363c0850
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-07-12 19:14:40 +0330

    first commit

---
+++ src/main/java/com/mohammadyousefi/ketabcheh/KetabchehApplication.java
@@ -0,0 +1,13 @@
+package com.mohammadyousefi.ketabcheh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KetabchehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KetabchehApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohammadyousefi/ketabcheh/KetabchehApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohammadyousefi.ketabcheh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KetabchehApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
