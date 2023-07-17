9d04e4bbc2f62e3bd6d53f31af8e34fd61be1952
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-07-17 22:43:58 +0330

    create cart item entity

---
+++ src/main/java/com/mohammadyousefi/ketabcheh/cartItem/CartItem.java
@@ -0,0 +1,27 @@
+package com.mohammadyousefi.ketabcheh.cartItem;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.mohammadyousefi.ketabcheh.book.Book;
+import com.mohammadyousefi.ketabcheh.user.User;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    private int quantity;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id")
+    @JsonIgnoreProperties({"cart"})
+    private User user;
+}
