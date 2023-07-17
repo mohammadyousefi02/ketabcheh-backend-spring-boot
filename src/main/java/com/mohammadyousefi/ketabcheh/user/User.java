a191303c2dc562f665a167166f630ccd5e933597
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-07-17 23:00:06 +0330

    add orders field

--- src/main/java/com/mohammadyousefi/ketabcheh/user/User.java
+++ src/main/java/com/mohammadyousefi/ketabcheh/user/User.java
@@ -2,6 +2,7 @@ package com.mohammadyousefi.ketabcheh.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.mohammadyousefi.ketabcheh.cartItem.CartItem;
+import com.mohammadyousefi.ketabcheh.order.Order;
 import com.mohammadyousefi.ketabcheh.profile.Profile;
 import com.mohammadyousefi.ketabcheh.save.Save;
 import jakarta.persistence.*;
@@ -33,4 +34,8 @@ public class User {
     @OneToMany(mappedBy = "user")
     @JsonIgnoreProperties({"user"})
     private List<Save> save;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnoreProperties({"user"})
+    private List<Order> orders;
 }
