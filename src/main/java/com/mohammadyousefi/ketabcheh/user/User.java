6c65a2f2499ad37ccca32d8f8b47fc9799bc865a
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-07-17 22:44:56 +0330

    add cart field

--- src/main/java/com/mohammadyousefi/ketabcheh/user/User.java
+++ src/main/java/com/mohammadyousefi/ketabcheh/user/User.java
@@ -1,9 +1,12 @@
 package com.mohammadyousefi.ketabcheh.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.mohammadyousefi.ketabcheh.cartItem.CartItem;
 import com.mohammadyousefi.ketabcheh.profile.Profile;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -21,4 +24,8 @@ public class User {
     @OneToOne(mappedBy = "user")
     @JsonIgnoreProperties({"user"})
     private Profile profile;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnoreProperties({"user"})
+    private List<CartItem> cart;
 }
