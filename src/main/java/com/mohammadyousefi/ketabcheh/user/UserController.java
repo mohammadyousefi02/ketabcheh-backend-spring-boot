c05d4709806aeb31495fd8fd0a4a6048cb28f98e
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-07-19 17:17:45 +0330

    create user controller

---
+++ src/main/java/com/mohammadyousefi/ketabcheh/user/UserController.java
@@ -0,0 +1,31 @@
+package com.mohammadyousefi.ketabcheh.user;
+
+import com.mohammadyousefi.ketabcheh.exception.NotFoundException;
+import com.mohammadyousefi.ketabcheh.response.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    private final UserServiceImpl userService;
+
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        Optional<User> userOptional = userService.findById(id);
+        if (userOptional.isEmpty()) throw new NotFoundException("there is no user with this id");
+        return userOptional.get();
+    }
+
+    @DeleteMapping("/{id}")
+    public Response<String> deleteById(@PathVariable Long id) {
+        String res = userService.deleteById(id);
+        return new Response<>(res, HttpStatus.OK.value());
+    }
+}
