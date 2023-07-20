2223cd6d42a6e8ae329674eeb101af4e79d033e6
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-07-20 23:10:21 +0330

    create AuthorRepository

---
+++ src/main/java/com/mohammadyousefi/ketabcheh/author/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.mohammadyousefi.ketabcheh.author;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<Author> findByName(String name);
+}
