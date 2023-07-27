7ccd7e166bccbfc6de8d34dd8f790a83051c5200
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-07-27 22:08:20 +0330

    create Comment controller

---
+++ src/main/java/com/mohammadyousefi/ketabcheh/comment/CommentController.java
@@ -0,0 +1,59 @@
+package com.mohammadyousefi.ketabcheh.comment;
+
+import com.mohammadyousefi.ketabcheh.auth.Authorization;
+import com.mohammadyousefi.ketabcheh.book.Book;
+import com.mohammadyousefi.ketabcheh.book.BookService;
+import com.mohammadyousefi.ketabcheh.exception.ErrorMessages;
+import com.mohammadyousefi.ketabcheh.exception.NotFoundException;
+import com.mohammadyousefi.ketabcheh.response.Response;
+import com.mohammadyousefi.ketabcheh.user.User;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/comments")
+public class CommentController {
+    private final CommentService commentService;
+    private final BookService bookService;
+
+    public CommentController(CommentService commentService, BookService bookService) {
+        this.commentService = commentService;
+        this.bookService = bookService;
+    }
+
+    @PostMapping("/{bookId}")
+    @Authorization
+    public Response<String> comment(HttpServletRequest request, @PathVariable Long bookId, @RequestBody CommentDto commentDto) {
+        Optional<Book> bookOptional = bookService.findById(bookId);
+        if (bookOptional.isEmpty()) throw new NotFoundException(ErrorMessages.notFound("book"));
+
+        return new Response<>(saveComment(request, bookOptional.get(), commentDto.getMessage(), null), HttpStatus.CREATED.value());
+    }
+
+    @PostMapping("/reply/{commentId}")
+    @Authorization
+    public Response<String> reply(HttpServletRequest request, @PathVariable Long commentId, @RequestBody CommentDto commentDto) {
+        Comment comment = commentService.findById(commentId);
+        return new Response<>(saveComment(request, comment.getBook(), commentDto.getMessage(), comment), HttpStatus.CREATED.value());
+    }
+
+    @DeleteMapping("/{commentId}")
+    @Authorization
+    public Response<String> deleteById(@PathVariable Long commentId) {
+        return new Response<>(commentService.deleteById(commentId));
+    }
+
+    private String saveComment(HttpServletRequest request, Book book, String message, Comment replyTo) {
+        Comment comment = new Comment();
+        comment.setBook(book);
+        User user = new User();
+        user.setId((Long) request.getAttribute("userId"));
+        comment.setUser(user);
+        comment.setMessage(message);
+        if (replyTo != null) comment.setReplyTo(replyTo);
+        return commentService.comment(comment);
+    }
+}
