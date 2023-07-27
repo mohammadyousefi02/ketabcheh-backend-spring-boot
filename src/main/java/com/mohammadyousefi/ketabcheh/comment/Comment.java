21af3cc37a0c4d8c74fe64b2a4200f030cb89ea1
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-07-27 22:08:05 +0330

    create Comment entity

---
+++ src/main/java/com/mohammadyousefi/ketabcheh/comment/Comment.java
@@ -0,0 +1,39 @@
+package com.mohammadyousefi.ketabcheh.comment;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.mohammadyousefi.ketabcheh.book.Book;
+import com.mohammadyousefi.ketabcheh.user.User;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    private String message;
+
+    @ManyToOne
+    @JoinColumn(name = "reply_to")
+    @JsonIgnoreProperties({"comments"})
+    private Comment replyTo;
+
+    @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties({"comment"})
+    private List<Comment> comments;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnoreProperties({"comments"})
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    @JsonIgnoreProperties({"comments"})
+    private Book book;
+}
