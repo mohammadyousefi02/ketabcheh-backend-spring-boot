0284ed53e13335211467d7c6d3f0f99b8748f0b9
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-08-08 03:15:09 +0330

    add min and max

--- src/main/java/com/mohammadyousefi/ketabcheh/auth/SignupDto.java
+++ src/main/java/com/mohammadyousefi/ketabcheh/auth/SignupDto.java
@@ -1,16 +1,19 @@
 package com.mohammadyousefi.ketabcheh.auth;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class SignupDto {
     @NotNull
+    @Min(value = 3)
     private String username;
     @NotNull
     @Email
     private String email;
     @NotNull
+    @Min(value = 8)
     private String password;
 }
