9221388b903660cd81d5c58da61c04482bff03b7
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-08-08 02:39:29 +0330

    handle bind exceptions

--- src/main/java/com/mohammadyousefi/ketabcheh/exception/ExceptionHandlers.java
+++ src/main/java/com/mohammadyousefi/ketabcheh/exception/ExceptionHandlers.java
@@ -1,14 +1,35 @@
 package com.mohammadyousefi.ketabcheh.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import java.util.Objects;
+
+@RestControllerAdvice
 public class ExceptionHandlers {
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ErrorResponse> handleException(Exception exception) {
-        ErrorResponse errorResponse = new ErrorResponse(exception.getMessage(), exception.getHttpStatus().value(), System.currentTimeMillis());
-        return new ResponseEntity<>(errorResponse, exception.getHttpStatus());
+        return generateErrorResponse(exception.getMessage(), exception.getHttpStatus());
+    }
+
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ErrorResponse> handleBindException(BindException ex) {
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+        ResponseStatus responseStatus = ex.getClass().getAnnotation(ResponseStatus.class);
+        if (responseStatus != null) {
+            httpStatus = responseStatus.value();
+        }
+        return generateErrorResponse(Objects.requireNonNull(ex.getFieldError()).getDefaultMessage(), httpStatus);
+    }
+
+    private ResponseEntity<ErrorResponse> generateErrorResponse(String message, HttpStatus httpStatus) {
+        ErrorResponse errorResponse = new ErrorResponse(message, httpStatus.value(), System.currentTimeMillis());
+        return new ResponseEntity<>(errorResponse, httpStatus);
     }
 }
