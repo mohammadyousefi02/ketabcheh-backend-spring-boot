f6357e4faa13d3274d29e2910685face2fec94b1
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-08-08 02:58:29 +0330

    create map for fieldErrors

--- src/main/java/com/mohammadyousefi/ketabcheh/exception/ExceptionHandlers.java
+++ src/main/java/com/mohammadyousefi/ketabcheh/exception/ExceptionHandlers.java
@@ -7,29 +7,32 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionHandlers {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+    public ResponseEntity<ErrorResponse<String>> handleException(Exception exception) {
         return generateErrorResponse(exception.getMessage(), exception.getHttpStatus());
     }
 
 
     @ExceptionHandler(BindException.class)
-    public ResponseEntity<ErrorResponse> handleBindException(BindException ex) {
+    public ResponseEntity<ErrorResponse<Map<String, String>>> handleBindException(BindException ex) {
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
         ResponseStatus responseStatus = ex.getClass().getAnnotation(ResponseStatus.class);
         if (responseStatus != null) {
             httpStatus = responseStatus.value();
         }
-        return generateErrorResponse(Objects.requireNonNull(ex.getFieldError()).getDefaultMessage(), httpStatus);
+        Map<String, String> errorsMap = new HashMap<>();
+        ex.getFieldErrors().forEach(fieldError -> errorsMap.put(fieldError.getField(), fieldError.getDefaultMessage()));
+        return generateErrorResponse(errorsMap, httpStatus);
     }
 
-    private ResponseEntity<ErrorResponse> generateErrorResponse(String message, HttpStatus httpStatus) {
-        ErrorResponse errorResponse = new ErrorResponse(message, httpStatus.value(), System.currentTimeMillis());
+    private <T> ResponseEntity<ErrorResponse<T>> generateErrorResponse(T message, HttpStatus httpStatus) {
+        ErrorResponse<T> errorResponse = new ErrorResponse<>(message, httpStatus.value(), System.currentTimeMillis());
         return new ResponseEntity<>(errorResponse, httpStatus);
     }
 }
