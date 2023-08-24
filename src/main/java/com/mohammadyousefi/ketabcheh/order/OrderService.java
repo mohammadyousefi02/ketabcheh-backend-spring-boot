2853a5fdcf4e26318599587a099fa2f82248c0d7
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-08-24 20:52:15 +0330

    add findByUserId

--- src/main/java/com/mohammadyousefi/ketabcheh/order/OrderService.java
+++ src/main/java/com/mohammadyousefi/ketabcheh/order/OrderService.java
@@ -1,6 +1,10 @@
 package com.mohammadyousefi.ketabcheh.order;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface OrderService {
+    List<Order> findByUserId(Long userId);
     Order order(OrderDto orderDto, Boolean useWallet);
     String toggleStatus(Long orderId);
 }
